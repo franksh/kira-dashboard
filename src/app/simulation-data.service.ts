0463869d2d0d737e47a141e06b6bc4189bc2f210
Adrian Zachariae <scazachariae@gmail.com>
2018-11-12 14:05:23 +0100

    simulation service observable added

--- src/app/simulation-data.service.ts
+++ src/app/simulation-data.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Observable, of, Subject } from "rxjs";
+
+import {
+  SimSnapshot,
+  SimulationResult
+} from "./display-control/simulation-result";
+import { SIMMOCKUP } from "../display-control/simulation-result-mockup-2";
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class SimulationDataService {
 
-  constructor() { }
+	private simulationresultSource = new Subject<SimulationResult>;
+	simulationresult$ = this.simulationresultSource.asObservable();
+
+  constructor() {
+  	this.simulationresultSource.next(SIMMOCKUP)
+  }
 }
