d4ed5a8061e19af75abdd06fc4ac8bba72ba9a1b
franksh <frnkschlssr@gmail.com>
2019-01-25 16:15:04 +0100

    fix bug in data loading

--- src/app/services/simulation-data.service.ts
+++ src/app/services/simulation-data.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { Observable, from } from "rxjs";
 import { Http } from "@angular/http";
-import { map } from "rxjs/operators";
+import { map, switchMap } from "rxjs/operators";
 
 import { SimulationResult } from "./simulation-result";
 import { SIMMOCKUP } from "./simulation-result-mockup";
@@ -11,52 +11,51 @@ import { SIMMOCKUP } from "./simulation-result-mockup";
 })
 export class SimulationDataService {
   simulationresult$: Observable<SimulationResult> = from([SIMMOCKUP]);
-  listts: number[];
+  // listts: number[];
   listts$: Observable<number[]>;
 
   // Load mock data
   loadMockData() {
     this.simulationresult$ = from([SIMMOCKUP]);
+
+    let listts: number[] = [];
+    for (var i in SIMMOCKUP.snapshots) {
+      listts.push(SIMMOCKUP.snapshots[i].timestamp);
+    }
+    // this.listts = listts;
+    this.listts$ = from([listts]);
   }
 
   // Load actual simulation data
   // stored in assets/data
   loadSimulationData() {
-    this.simulationresult$ = this.http
-      .get("http://localhost:4200/assets/data/Simulation_results_N100_T76.json")
-      .pipe(
-        map(response => {
-          let simResult = new SimulationResult(response.json());
-          return simResult;
-        })
-      );
-  }
-
-  // This method works
-  initializeMockTimescale() {
-    this.listts = [];
-    for (var i in SIMMOCKUP.snapshots) {
-      this.listts.push(SIMMOCKUP.snapshots[i].timestamp);
-    }
-    this.listts$ = from([this.listts]);
-  }
-
-  // This does not work, why?
-  initializeTimescale() {
-    this.simulationresult$.subscribe(simResults => {
-      this.listts = [];
-      for (var i in simResults.snapshots) {
-        this.listts.push(simResults.snapshots[i].timestamp);
-      }
-      this.listts$ = from([this.listts]);
-    });
+    let data$ = this.http.get(
+      // "http://localhost:4200/assets/data/Simulation_results_N100_T76.json"
+      "http://localhost:4200/assets/data/Kira_trajectories_N1000_T168_real.json"
+    );
+    // Store simulation snapshots
+    this.simulationresult$ = data$.pipe(
+      map(response => {
+        let simResult = new SimulationResult(response.json());
+        return simResult;
+      })
+    );
+    // Store time series
+    this.listts$ = data$.pipe(
+      switchMap(response => {
+        let simResult = new SimulationResult(response.json());
+        let listts: number[] = [];
+        for (var i in simResult.snapshots) {
+          listts.push(simResult.snapshots[i].timestamp);
+        }
+        // this.listts = listts;
+        return from([listts]);
+      })
+    );
   }
 
   constructor(private http: Http) {
     // this.loadMockData();
     this.loadSimulationData();
-
-    // this.initializeTimescale();
-    this.initializeMockTimescale();
   }
 }
